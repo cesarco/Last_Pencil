6c1f917a3b8f39d49008709158f55bd9c57c1148
cesarco <devcesarco@outlook.es>
2022-07-10 18:32:24 -0500

    Stage 2/5: New rules

--- Last Pencil/task/game.py
+++ Last Pencil/task/game.py
@@ -1,1 +1,12 @@
-print('||||||||\n Your turn!')
+print('How many pencils would you like to use:')
+num_1 = int(input())
+
+print('Who will be the first (John, Jack)')
+user = input()
+
+if user == 'John':
+    print(num_1 * "|")
+    print(user + " is going first")
+elif user == 'Jack':
+    print(num_1 * "|")
+    print(user + " is going first")
