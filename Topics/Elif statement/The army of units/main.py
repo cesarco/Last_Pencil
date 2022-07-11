88d3c47c73b609fcf00fadddcea973925eec43d6
cesarco <devcesarco@outlook.es>
2022-07-10 19:28:15 -0500

    The army of units

---
+++ Topics/Elif statement/The army of units/main.py
@@ -0,0 +1,14 @@
+number = int(input())
+
+if number < 1:
+    print('no army')
+elif number >= 1 and number <= 9:
+    print('few')
+elif number >= 10 and number <= 49:
+    print('pack')
+elif number >= 50 and number <= 499:
+    print('horde')
+elif number >= 500 and number <= 999:
+    print('swarm')
+else:
+    print('legion')
